75568dd9a5c6c5dcc77ad8d9ed19aa941bfe5ca8
Aravind K Krishna <ananthkkrishna2001@gmail.com>
2024-06-29 09:43:12 +0530

    design initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var state={
+taskList:[
+
+    {
+        taskTitle :"",
+        taskType : "",
+          task_desc : "",
+        imageurl :""
+      
+    },
+    {
+        taskTitle :"",
+        taskType : "",
+          task_desc : "",
+        imageurl :""
+      
+    },
+    {
+        taskTitle :"",
+        taskType : "",
+          task_desc : "",
+        imageurl :""
+      
+    },
+    {
+        taskTitle :"",
+        taskType : "",
+          task_desc : "",
+        imageurl :""
+      
+    }
+  
+]
+}
+const state ={
+    taskList:[],
+};
+//dom operation
+const taskContent=document.querySelector(".task_contents");
+const taskModel=document.querySelector(". task_model_body");
+console.log(taskContent);
+console.log(taskModel);
